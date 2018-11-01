d6e830b0af10386d1370e22059381d111f848932
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-01 12:20:59 -0300

    Add files via upload

---
+++ Biblioteca/src/Entity/Pessoa.java
@@ -0,0 +1,34 @@
+package Entity;
+/*
+ * @autor :Marcio Alexandre Freire Sindeaux
+ */
+public class Pessoa {
+	private String 			nome;
+	private Integer 		idade;
+	private String 			cpf;
+	private String			telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
